41338a9a6db8def3d951d9633d1f470a8e813e99
folicacidb <folicacidb@gmail.com>
2024-09-10 13:29:55 +0900

    Add files via upload

---
+++ 003_practice/mainfile3.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n = 125;
+	printf("10진수\t%3d\n", n);
+	printf(" 8진수\t%3o\n", n);
+	printf("16진수\t%3x\n", n);
+}
+
---
+++ 004_practice/mainfile4.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char ch = 'A'; //문자는 작은 따옴표
+    char str[20] = "Hello World!"; //문자열은 큰 따옴표
+    float flt = 10.234;
+    int no = 150;
+    double dbl = 20.123456;
+
+    printf("Character is %c\n", ch);
+    printf("String is %s\n", str);
+    printf("Floating Vaule is %f\n", flt);
+    printf("Integer Vaule is %d\n", no);
+    printf("Double Vaule is %f\n", dbl);
+    printf("Octal Vaule is %o\n", no);
+    printf("Hexadecimal Vaule is %x\n", no);
+}
